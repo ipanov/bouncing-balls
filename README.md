895706bbf47d517a7aabfdc804ad2bf79b6d573e
Ilija Panov <ilijapanov83@gmail.com>
2019-12-29 04:38:25 +0100

    setting up project structure, added canvas

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# bouncing-balls
-technical assignment for hfour interview process
+# Bouncing balls
+
+Bouncing balls is an application spanning balls on each mouse click event using an HTML5 Canvas.
+Balls are affected by gravity and friction and have a predetermined elasticity.
+Let's asume that the balls are moving on different planes along an imaginary z-axis so the possibility of them coliding is exluded.
+
+Bouncing ball favicon.ico by Angelo Troiano from the Noun Project
+<https://thenounproject.com/term/bouncing-ball/1612771/>
+
+## Getting Started
+
+1. Clone the repo:
+
+        git clone https://github.com/ipanov/bouncing-balls.git
+
+2. Install dependencies:
+
+        yarn
+
+    or
+
+        npm install
+
+3. Run webpack:
+
+        npm start
+
+The bouncing balls application should open up automatically at http://localhost:3000
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isDevelopment = process.env.NODE_ENV === 'development'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/canvas.js',
+  output: {
+    path: __dirname + '/dist/',
+    filename: './js/canvas.bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: {
+        baseDir: ['dist']
+      },
+      files: ['./dist/*'],
+      notify: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      favicon: 'favicon.ico',
+      template: 'src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    })
+  ],
+  watch: true,
+  devtool: 'source-map'
+}
